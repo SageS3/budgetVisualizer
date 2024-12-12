033fed9dff73d521a36f4cccda90fc3ffed0368c
Sage Souza <sage.souza333@gmail.com>
2024-12-11 23:55:26 -0800

    Add state to money form

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import TotalDisplay from "./TotalDisplay"
 import { useState } from "react"
 
 function App() {
-  const [formTotal, setFormTotal] = useState(0.0)
+  const [formData, setFormData] = useState({})
+
   function formSubmitHandler() {}
 
   return (
     <div className="App">
-      <MoneyForm formTotal={formTotal} />
-      <TotalDisplay />
+      <MoneyForm />
+      <TotalDisplay formData={formData} />
     </div>
   )
 }
--- src/MoneyForm.js
+++ src/MoneyForm.js
@@ -1,17 +1,26 @@
 import { useState } from "react"
 import "./MoneyForm.css"
 
-function MoneyForm({ formTotal }) {
+function MoneyForm() {
   // when form submits- updates state -> UI is updated in APP which takes
   // state as props and displays it.
+  const [formData, setFormData] = useState({
+    category: "",
+    amount: 0,
+    type: "Expense",
+  })
+
+  function submitHandler() {}
+
+  function onChangeHandler() {}
 
   return (
     <form className="form-body">
-      <select>
-        <option>Income</option>
-        <option>Expense</option>
+      <select onChange={() => {}} value={formData.type}>
+        <option value={"Expense"}>Expense</option>
+        <option value={"Income"}>Income</option>
       </select>
-      <input type="money" placeholder={formTotal}></input>
+      <input placeholder={"Amount"}></input>
       <input placeholder="Category"></input>
       <button>Submit</button>
     </form>
